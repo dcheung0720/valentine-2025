f67ed60eae08c0bfa05472f3c5d36cbd652fb206
David Cheung <davidcheung231@gmail.com>
2025-02-11 12:49:35 -0600

    on click added to images

--- src/components/Selection.js
+++ src/components/Selection.js
@@ -9,28 +9,30 @@ const Selection = ({title, options, setCarousel}) =>{
 
     const [optionSelected, setOptionSelected] = useState([]);
 
+    const handleOnClickOption = (option) =>{
+        if (optionSelected.includes(option)){
+            setOptionSelected(optionSelected.filter(item => item !== option));
+        }
+        else{
+            setOptionSelected([...optionSelected, option])
+        }
+    };
+
     return (
         <div style = {{width: "1200px", borderRadius: "10px", textAlign: "center", marginBottom: "150px", alignItems: "center"}}>
             <h2 style={{marginTop: "30px"}}>{title}</h2>
             <div style = {{display: "flex", justifyContent: "center", flexWrap: "wrap", alignItems: "center", alignContent: "center"}}>
                 {options.map(option =>  
-                    <div style={{margin: "10px",}}> 
-                        <div>
-                            <Image src={`/valentine-2025/images/${option}.jpg`} style = {{width: "150px", height: "150px"}}  />
+                    <div style={{margin: "10px",}} > 
+                        <div onClick={() => handleOnClickOption(option)}>
+                            <Image src={`/valentine-2025/images/${option}.jpg`}  style = {{width: "150px", height: "150px"}}  />
                         </div>
                         <div>
                             <Form.Check
                                 inline
                                 label={option}
                                 name="group1"
-                                onClick={() => {
-                                    if (optionSelected.includes(option)){
-                                        setOptionSelected(optionSelected.filter(item => item !== option));
-                                    }
-                                    else{
-                                        setOptionSelected([...optionSelected, option])
-                                    }
-                                }}      
+                                onClick={() => handleOnClickOption(option)}      
                             />
                         </div>
                     </div> 
